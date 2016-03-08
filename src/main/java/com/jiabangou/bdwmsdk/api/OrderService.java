83ebd62ea3434fa2cd2b5859bf1aef08f17ba4fc
huwei <huwei@eking.mobi>
2016-03-08 10:51:16 +0800

    创建订单服务

---
+++ src/main/java/com/jiabangou/bdwmsdk/api/OrderService.java
@@ -0,0 +1,7 @@
+package com.jiabangou.bdwmsdk.api;
+
+/**
+ * Created by freeway on 16/3/8.
+ */
+public interface OrderService {
+}
---
+++ src/main/java/com/jiabangou/bdwmsdk/api/impl/OrderServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jiabangou.bdwmsdk.api.impl;
+
+/**
+ * Created by freeway on 16/3/8.
+ */
+public class OrderServiceImpl {
+}
