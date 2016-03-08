0e43c7b94de76eaede303f6bd629bd91f400c12e
wanglei <wanglei@liziba.com>
2016-03-08 19:24:07 +0800

    添加注释

--- src/main/java/com/jiabangou/bdwmsdk/api/DishService.java
+++ src/main/java/com/jiabangou/bdwmsdk/api/DishService.java
@@ -6,26 +6,123 @@ import com.sun.tools.javac.util.List;
 
 public interface DishService extends BdWmService {
 
+    /**
+     * <pre>
+     * 创建菜品分类信息
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_category_create
+     * </pre>
+     * @param category 菜品分类信息
+     * @return 菜品分类id
+     * @throws BdWmErrorException
+     */
     long createCategory(Category category) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 更新菜品分类信息
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_category_update
+     * </pre>
+     * @param categoryUpdate 菜品信息
+     * @throws BdWmErrorException
+     */
     void updateCategory(CategoryUpdate categoryUpdate) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 获取单个菜品分类信息
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_category_get
+     * </pre>
+     * @param shopId 商户id
+     * @param name 分类的名字
+     * @return 菜品分类信息
+     * @throws BdWmErrorException
+     */
     Category getCategory(String shopId, String name) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 获取所有菜品分类信息
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_category_all
+     * </pre>
+     * @param shopId 商户id
+     * @return 菜品分类列表
+     * @throws BdWmErrorException
+     */
     List<Category> getAllCategory(String shopId) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品上传
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_create
+     * </pre>
+     * @param dish 菜品信息
+     * @throws BdWmErrorException
+     */
     void create(Dish dish) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品修改
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_update
+     * </pre>
+     * @param dish 菜品信息
+     * @throws BdWmErrorException
+     */
     void update(Dish dish) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品上线
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_online
+     * </pre>
+     * @param shopId 商户id
+     * @param dishId 菜品id
+     * @throws BdWmErrorException
+     */
     void online(String shopId, String dishId) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品下线
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_offline
+     * </pre>
+     * @param shopId 商户id
+     * @param dishId 菜品id
+     * @throws BdWmErrorException
+     */
     void offline(String shopId, String dishId) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品删除
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_delete
+     * </pre>
+     * @param shopId 商户id
+     * @param dishId 菜品id
+     * @throws BdWmErrorException
+     */
     void delete(String shopId, String dishId) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品库存阈值设置
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_threshold_set
+     * </pre>
+     * @param shopId 商户id
+     * @param dishId 菜品id
+     * @param thresholds 库存阈值
+     * @throws BdWmErrorException
+     */
     void setThreshold(String shopId, String dishId, List<Threshold> thresholds) throws BdWmErrorException;
 
+    /**
+     * <pre>
+     * 菜品批量替换
+     * 详情请见: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_dish_replace_batch
+     * </pre>
+     * @param dishReplaceBatch 批量菜品对象
+     * @throws BdWmErrorException
+     */
     void replaceBatch(DishReplaceBatch dishReplaceBatch) throws BdWmErrorException;
 
 }
--- src/main/java/com/jiabangou/bdwmsdk/api/OrderService.java
+++ src/main/java/com/jiabangou/bdwmsdk/api/OrderService.java
@@ -2,12 +2,9 @@ package com.jiabangou.bdwmsdk.api;
 
 import com.jiabangou.bdwmsdk.exception.BdWmErrorException;
 import com.jiabangou.bdwmsdk.model.OrderDetail;
-<<<<<<< Updated upstream
 import com.jiabangou.bdwmsdk.model.OrderLite;
 import com.jiabangou.bdwmsdk.model.Page;
-=======
 import com.jiabangou.bdwmsdk.model.OrderStatus;
->>>>>>> Stashed changes
 
 public interface OrderService extends BdWmService {
 
@@ -26,9 +23,9 @@ public interface OrderService extends BdWmService {
      * type和String参考: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_appendix
      * 可以从 OrderCancelType 提取 type 和 reason
      * </pre>
-     * @param orderId
-     * @param type
-     * @param reason
+     * @param orderId 订单id
+     * @param type 订单取消原因分类
+     * @param reason 订单取消原因描述
      */
     void cancel(String orderId, int type, String reason) throws BdWmErrorException;
 
@@ -38,17 +35,18 @@ public interface OrderService extends BdWmService {
      * 提供给合作方反馈订单状态为完成所用。确认订单后才能完成订单。 注：1、非完成态的订单不予结算。2、百度配送订单由骑士完成。
      * 参考: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_order_complete
      * </pre>
-     * @param orderId
+     * @param orderId 订单id
      * @throws BdWmErrorException
      */
     void complete(String orderId) throws BdWmErrorException;
 
     /**
      * <pre>
-     *
+     * 查看订单状态
+     * 参考: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_order_status_get
      * </pre>
-     * @param orderId
-     * @return
+     * @param orderId 订单id
+     * @return 订单状态码
      * @throws BdWmErrorException
      */
     int getStatus(String orderId) throws BdWmErrorException;
@@ -59,8 +57,8 @@ public interface OrderService extends BdWmService {
      * 提供给合作方查看订单详情所用。
      * 参考: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_order_get
      * </pre>
-     * @param orderId
-     * @return
+     * @param orderId 订单id
+     * @return 订单详情
      * @throws BdWmErrorException
      */
     OrderDetail getOrderDetail(String orderId) throws BdWmErrorException;
@@ -71,7 +69,7 @@ public interface OrderService extends BdWmService {
      * 提供给合作方查看订单列表所用。
      * 参考: http://api.waimai.baidu.com/openapi/doc/interface?md=API2.0_order_list
      * </pre>
-     * @param orderId
+     * @param orderId 订单id
      * @return
      * @throws BdWmErrorException
      */
