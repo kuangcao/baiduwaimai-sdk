7dfc81f76242c3ab164c2732fde1b43fb08fbc1c
huwei <huwei@eking.mobi>
2016-03-09 21:23:54 +0800

    百度外卖客户端加入getXXXService

--- src/main/java/com/jiabangou/bdwmsdk/api/BdWmClient.java
+++ src/main/java/com/jiabangou/bdwmsdk/api/BdWmClient.java
@@ -5,4 +5,22 @@ public interface BdWmClient {
 
     void setBaiduWaimaiConfigStorage(BdWmConfigStorage baiduWaimaiConfigStorage);
 
+    /**
+     * 获取门店服务
+     * @return
+     */
+    ShopService getShopService();
+
+    /**
+     * 获取菜品服务
+     * @return
+     */
+    DishService getDishService();
+
+    /**
+     * 获取订单服务
+     * @return
+     */
+    OrderService getOrderService();
+
 }
